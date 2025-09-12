8319776bcada01dacd963f104b192113aa15d731
Tubagus Aldi <tubagusaldi28@gmail.com>
2025-09-12 14:08:14 +0700

    feat: menambahkan interface AuthServiceImpl, EventServiceImpl, User ServiceImpl, dan ValidatorServiceImpl

---
+++ src/main/java/com/sinaukoding/event_booking_system/service/managementuser/impl/UserServiceImpl.java
@@ -0,0 +1,143 @@
+package com.sinaukoding.event_booking_system.service.managementuser.impl;
+
+import com.sinaukoding.event_booking_system.builder.CustomBuilder;
+import com.sinaukoding.event_booking_system.entity.managementuser.User;
+import com.sinaukoding.event_booking_system.mapper.managementuser.UserMapper;
+import com.sinaukoding.event_booking_system.model.app.AppPage;
+import com.sinaukoding.event_booking_system.model.app.SimpleMap;
+import com.sinaukoding.event_booking_system.model.enums.Role;
+import com.sinaukoding.event_booking_system.model.enums.Status;
+import com.sinaukoding.event_booking_system.model.filter.UserFilterRecord;
+import com.sinaukoding.event_booking_system.model.request.RegisterRequestRecord;
+import com.sinaukoding.event_booking_system.model.request.UserRequestRecord;
+import com.sinaukoding.event_booking_system.repository.managementuser.UserRepository;
+import com.sinaukoding.event_booking_system.service.managementuser.UserService;
+import com.sinaukoding.event_booking_system.util.FilterUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void add(UserRequestRecord request) {
+        // validasi mandatory
+        validasiMandatory(request);
+
+        // validasi data existing
+        if (userRepository.existsByEmail(request.email().toLowerCase())) {
+            throw new RuntimeException("Email [" + request.email() + "] sudah digunakan");
+        }
+        if (userRepository.existsByUsername(request.username().toLowerCase())) {
+            throw new RuntimeException("Username [" + request.username() + "] sudah digunakan");
+        }
+
+        var user = userMapper.requestToEntity(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public void edit(UserRequestRecord request) {
+        // validasi mandatory
+        validasiMandatory(request);
+
+        var userExisting = userRepository.findById(request.id()).orElseThrow(() ->  new RuntimeException("Data user tidak ditemukan"));
+
+        // validasi data existing
+        if (userRepository.existsByEmailAndIdNot(request.email().toLowerCase(), request.id())) {
+            throw new RuntimeException("Email [" + request.email() + "] sudah digunakan");
+        }
+        if (userRepository.existsByUsernameAndIdNot(request.username().toLowerCase(),  request.id())) {
+            throw new RuntimeException("Username [" + request.username() + "] sudah digunakan");
+        }
+
+        var user = userMapper.requestToEntity(request);
+        user.setId(userExisting.getId());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public Page<SimpleMap> findAll(UserFilterRecord filterRequest, Pageable pageable) {
+        CustomBuilder<User> builder = new CustomBuilder<>();
+
+        FilterUtil.builderConditionNotBlankLike("nama", filterRequest.nama(), builder);
+        FilterUtil.builderConditionNotBlankLike("email", filterRequest.email(), builder);
+        FilterUtil.builderConditionNotBlankLike("username", filterRequest.username(), builder);
+        FilterUtil.builderConditionNotNullEqual("status", filterRequest.status(), builder);
+        FilterUtil.builderConditionNotNullEqual("role", filterRequest.role(), builder);
+
+        Page<User> listUser = userRepository.findAll(builder.build(), pageable);
+        List<SimpleMap> listData = listUser.stream().map(user -> {
+            SimpleMap data = new SimpleMap();
+            data.put("id", user.getId());
+            data.put("nama", user.getNama());
+            data.put("username", user.getUsername());
+            data.put("email", user.getEmail());
+            data.put("role", user.getStatus().getLabel());
+            data.put("status", user.getRole().getLabel());
+            return data;
+        }).toList();
+
+        return AppPage.create(listData, pageable, listUser.getTotalElements());
+    }
+
+    @Override
+    public SimpleMap findById(String id) {
+        var user = userRepository.findById(id).orElseThrow(() ->  new RuntimeException("Data user tidak ditemukan"));
+        SimpleMap data = new SimpleMap();
+        data.put("id", user.getId());
+        data.put("nama", user.getNama());
+        data.put("username", user.getUsername());
+        data.put("email", user.getEmail());
+        data.put("status", user.getStatus().getLabel());
+        data.put("role", user.getRole().getLabel());
+        return data;
+    }
+
+    private void validasiMandatory(UserRequestRecord request) {
+        if (request.nama() == null || request.nama().isEmpty()) {
+            throw new RuntimeException("Nama tidak boleh kosong");
+        }
+        if (request.username() == null || request.username().isEmpty()) {
+            throw new RuntimeException("Username tidak boleh kosong");
+        }
+        if (request.email() == null || request.email().isEmpty()) {
+            throw new RuntimeException("Email tidak boleh kosong");
+        }
+        if (request.password() == null || request.password().isEmpty()) {
+            throw new RuntimeException("Email tidak boleh kosong");
+        }
+        if (request.status() == null) {
+            throw new RuntimeException("Status tidak boleh kosong");
+        }
+        if (request.role() == null) {
+            throw new RuntimeException("Role tidak boleh kosong");
+        }
+    }
+
+    @Override
+    public void register(RegisterRequestRecord request) {
+        UserRequestRecord newUserRequest = new UserRequestRecord(
+                null,
+                request.nama(),
+                request.username(),
+                request.email(),
+                request.password(),
+                Status.AKTIF,
+                Role.PESERTA
+        );
+        this.add(newUserRequest);
+    }
+}
