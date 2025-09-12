5cf7ac39e16b37546fc8905adb9eacf7b8d6acc2
Tubagus Aldi <tubagusaldi28@gmail.com>
2025-09-12 14:16:41 +0700

    fix: menambahkan request record gamabr

--- src/main/java/com/sinaukoding/event_booking_system/model/request/EventRequestRecord.java
+++ src/main/java/com/sinaukoding/event_booking_system/model/request/EventRequestRecord.java
@@ -2,9 +2,11 @@ package com.sinaukoding.event_booking_system.model.request;
 
 import com.sinaukoding.event_booking_system.model.enums.StatusAcara;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public record EventRequestRecord(String id,
                                  @NotBlank(message = "Nama Acara tidak boleh kosong")String namaAcara,
@@ -14,5 +16,6 @@ public record EventRequestRecord(String id,
                                  @NotNull(message = "Harga tiket tidak boleh kosong")Double hargaTiket,
                                  @NotNull(message = "Kapasitas tidak boleh kosong")Integer kapasitas,
                                  @NotNull(message = "Status tidak boleh kosong") StatusAcara statusAcara,
-                                 @NotBlank(message = "Lokasi tidak boleh kosong")String lokasi) {
+                                 @NotBlank(message = "Lokasi tidak boleh kosong")String lokasi,
+                                 @NotEmpty(message = "Gambar tidak boleh kosong") Set<String> listImage) {
 }
